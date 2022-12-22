ecf58a14316f8b1d9559d52169abcf1db9cea450
Pankaj Rayal <pankajrayal@gmail.com>
2022-12-22 12:54:31 -0600

    06-Routing in App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+import Detail from "./pages/Detail";
+import AddEditBlog from "./pages/AddEditBlog";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+
 function App() {
   return (
     <div className="App">
-      <Home />
+      <ToastContainer />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/create" element={<AddEditBlog />} />
+        <Route path="/update/:id" element={<AddEditBlog />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
