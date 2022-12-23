f171000664425460c3a341d856458fb932739b7d
Pankaj Rayal <pankajrayal@gmail.com>
2022-12-23 11:15:12 -0600

    16-Working on Blog Form

--- src/pages/AddEditBlog.js
+++ src/pages/AddEditBlog.js
@@ -1,9 +1,133 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import ReactTagInput from "@pathofdev/react-tag-input";
+import "@pathofdev/react-tag-input/build/index.css";
 
-function AddEditBlog() {
+const initialState = {
+  title: "",
+  tags: [],
+  trending: "no",
+  category: "",
+  description: "",
+};
+
+const categoryOptions = [
+  "Fashion",
+  "Technology",
+  "Food",
+  "Politics",
+  "Sports",
+  "Business",
+];
+
+const AddEditBlog = () => {
+  const [form, setForm] = useState(initialState);
+  const [file, setFile] = useState(null);
+
+  const { title, tags, category, trending, description } = form;
+
+  const handleChange = (e) => {};
+
+  const handleTags = (e) => {};
+
+  const handleTrending = (e) => {};
+
+  const onCategoryChange = () => {};
+
   return (
-    <div>AddEditBlog</div>
-  )
-}
+    <div className="container-fluid mb-4">
+      <div className="container">
+        <div className="col-12">
+          <div className="text-center heading py-2">Create Blog</div>
+        </div>
+        <div className="row h-100 justify-content-center align-items-center">
+          <div className="col-10 col-md-8 col-lg-6">
+            <form className="row blog-form">
+              <div className="col-12 py-3">
+                <input
+                  type="text"
+                  className="form-control input-text-box"
+                  placeholder="Title"
+                  name="title"
+                  value={title}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="col-12 py-3">
+                <ReactTagInput
+                  tags={tags}
+                  placeholder="Tags"
+                  onChange={handleTags}
+                />
+              </div>
+              <div className="col-12 py-3">
+                <p className="trending">Is is trending blog?</p>
+                <div className="form-check-inline mx-2">
+                  <input
+                    type="radio"
+                    className="form-check-input"
+                    value="yes"
+                    name="radioOption"
+                    checked={trending === "yes"}
+                    onChange={handleTrending}
+                  />
+                  <label className="form-check-label" htmlFor="radioOption">
+                    Yes&nbsp;
+                  </label>
+                  <input
+                    type="radio"
+                    className="form-check-input"
+                    value="no"
+                    name="radioOption"
+                    checked={trending === "no"}
+                    onChange={handleTrending}
+                  />
+                  <label className="form-check-label" htmlFor="radioOption">
+                    No
+                  </label>
+                </div>
+              </div>
+              <div className="col-12 py-3">
+                <select
+                  className="catg-dropdown"
+                  value={category}
+                  onChange={onCategoryChange}
+                >
+                  <option>Please select Category</option>
+                  {categoryOptions.map((category, index) => (
+                    <option value={category || ""} key={index}>
+                      {category}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="col-12 py-3">
+                <textarea
+                  className="form-control description-box"
+                  placeholder="Description"
+                  value={description}
+                  name="description"
+                  onChange={handleChange}
+                />
+              </div>
 
-export default AddEditBlog
+              <div className="mb-3">
+                <input
+                  className="form-control"
+                  type="file"
+                  onChange={(e) => setFile(e.target.files[0])}
+                />
+              </div>
+              <div className="col-12 py-3 text-center">
+                <button className="btn btn-add" type="submit">
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddEditBlog;
