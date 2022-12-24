1c13f56c2863d2f1e74693b65150131f0d863c0b
Pankaj Rayal <pankajrayal@gmail.com>
2022-12-24 13:07:22 -0600

    29-Using owl carousel to show Trending section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from "react";
 import BlogSection from "../components/BlogSection";
-import { collection, deleteDoc, onSnapshot } from "firebase/firestore";
+import {
+  collection,
+  deleteDoc,
+  getDocs,
+  onSnapshot,
+  query,
+  where,
+} from "firebase/firestore";
 import { db } from "../firebase";
 import { doc } from "firebase/firestore";
 import { toast } from "react-toastify";
@@ -8,18 +15,33 @@ import { toast } from "react-toastify";
 import Spinner from "../components/Spinner";
 import Tags from "../components/Tags";
 import MostPopular from "../components/MostPopular";
+import Trending from "../components/Trending";
 
 const Home = ({ setActive, user }) => {
   const [loading, setLoading] = useState(true);
   const [blogs, setBlogs] = useState([]);
   const [tags, setTags] = useState([]);
+  const [trendBlogs, setTrendBlogs] = useState([]);
+
+  const getTrendingBlogs = async () => {
+    const blogRef = collection(db, "blogs");
+    const trendQuery = query(blogRef, where("trending", "==", "yes"));
+    const querySnapshot = await getDocs(trendQuery);
+    let trendBlogs = [];
+    querySnapshot.forEach((doc) => {
+      trendBlogs.push({ id: doc.id, ...doc.data() });
+    });
+    setTrendBlogs(trendBlogs);
+  };
 
   useEffect(() => {
+    getTrendingBlogs();
     const unsub = onSnapshot(
       collection(db, "blogs"),
       (snapshot) => {
         let list = [];
         let tags = [];
+
         snapshot.docs.forEach((doc) => {
           tags.push(...doc.get("tags"));
           list.push({ id: doc.id, ...doc.data() });
@@ -36,6 +58,7 @@ const Home = ({ setActive, user }) => {
     );
     return () => {
       unsub();
+      getTrendingBlogs();
     };
   }, []);
 
@@ -60,7 +83,7 @@ const Home = ({ setActive, user }) => {
     <div className="container-fluid pb-4 pt-4 padding">
       <div className="container padding">
         <div className="row mx-0">
-          <h2>Trending</h2>
+          <Trending blogs={trendBlogs} />
           <div className="col-md-8">
             <BlogSection
               blogs={blogs}
@@ -70,7 +93,7 @@ const Home = ({ setActive, user }) => {
           </div>
           <div className="col-md-3">
             <Tags tags={tags} />
-            <MostPopular blogs={blogs} />>
+            <MostPopular blogs={blogs} />
           </div>
         </div>
       </div>
